7149e5032c6c0b6d217a1f812a9887c7b0e29363
eckon <niklas.meyer95@hotmail.de>
2020-04-14 18:59:11 +0200

    Remove tls
    This is being handled by Heroku now

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { readFileSync, existsSync } from 'fs';
 
 async function bootstrap() {
-  const keyPath = '/etc/letsencrypt/live/eckon.me/privkey.pem';
-  const certPath = '/etc/letsencrypt/live/eckon.me/fullchain.pem';
-  const hasFilesForTLS = existsSync(keyPath) && existsSync(certPath);
-  console.log(`Has files for TLS: ${hasFilesForTLS}`);
-
-  let initOptions = {};
-
-  if (hasFilesForTLS) {
-    const httpsOptions = {
-      key: readFileSync(keyPath),
-      cert: readFileSync(certPath),
-    };
-    initOptions = { httpsOptions };
-  }
-
-  const app = await NestFactory.create(AppModule, initOptions);
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
 
   const options = new DocumentBuilder()
